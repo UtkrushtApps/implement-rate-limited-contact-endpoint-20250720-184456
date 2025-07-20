1e519d6697f441e70e223bcfd07432ef87f71c6c
Naman Muley <naman.g.muley@gmail.com>
2025-07-20 18:45:18 +0530

    Add run script

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-set -e
-
-bash install.sh
-
-echo "[run.sh] API container started. To check status, run: docker ps"
-echo "[run.sh] Try: curl http://localhost:8000/employees"
+bash run.sh
